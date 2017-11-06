bac1a6f7715e67874149343fa7f0af41ebdb1f40
luca-zamboni <lucazamboni92@gmail.com>
2017-11-06 21:35:12 +0100

    firebase almost

--- index.js
+++ index.js
@@ -9,22 +9,29 @@ var app_server_module_ngfactory_1 = require("./dist/out-tsc/dist/ngfactory/src/a
 var express = require("express");
 var fs_1 = require("fs");
 var path_1 = require("path");
-var PORT = 4000;
+var PORT = 80;
 core_1.enableProdMode();
 var app = express();
 var template = fs_1.readFileSync(path_1.join(__dirname, 'dist', 'index.html')).toString();
-app.engine('html', function (_, options, callback) {
-    var opts = { document: template, url: options.req.url };
+app.engine('html', function(_, options, callback) {
+    var opts = {
+        document: template,
+        url: options.req.url
+    };
     platform_server_1.renderModuleFactory(app_server_module_ngfactory_1.AppServerModuleNgFactory, opts)
-        .then(function (html) { return callback(null, html); });
+        .then(function(html) {
+            return callback(null, html);
+        });
 });
 app.set('view engine', 'html');
 app.set('views', 'src');
-app.get('*.*', express.static(path_1.join(__dirname, '..', 'dist')));
-app.get('*', function (req, res) {
-    res.render('index', { req: req });
+app.get('*.*', express.static(path_1.join(__dirname, 'dist')));
+app.get('*', function(req, res) {
+    res.render('index', {
+        req: req
+    });
 });
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log("listening on http://localhost:" + PORT + "!");
 });
 
